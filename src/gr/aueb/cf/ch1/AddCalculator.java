515746de422a66bef1c313ab25c7e3a34fdc42f0
Nektarios_Tsagkaris <nektarios.tsagkaris@gmail.com>
2024-12-16 20:01:16 +0200

    hotfix - println added

--- src/gr/aueb/cf/ch1/AddCalculator.java
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -1,21 +1,22 @@
 package gr.aueb.cf.ch1;
 
 
+public class AddCalculator {
 
-    public class AddCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = 0;
 
-        public static void main(String[] args) {
-            int num1 = 10;
-            int num2 = 20;
-            int result = 0;
+        result = add(num1, num2);
 
-            result = add(num1, num2);
+        System.out.println("Sum " + result);
 
-        }
+    }
 
-        public static int add(int a, int b) {
-            return a + b;
+    public static int add(int a, int b) {
+        return a + b;
 
-        }
+    }
 
 }
